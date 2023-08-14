6c94c00b3b915d96e217934f8d7d7fde76a1d73c
desktop <badtripwithurlxve@gmail.com>
2023-08-14 19:33:29 +0300

    V1

---
+++ Triangle.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace figure_area
+{
+    public class Triangle : Shape
+    {
+        public double Osnov { get; set; }
+        public double Height { get; set; }
+
+        public Triangle(double osnov, double height)
+        {
+            Osnov = osnov;
+            Height = height;
+        }
+
+        public override double CalculateArea()
+        {
+            return 0.5 * Osnov * Height;
+        }
+    }
+}
